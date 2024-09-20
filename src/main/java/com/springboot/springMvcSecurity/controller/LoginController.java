3a155d27312a322667e0423162f83d2c476ba6cf
Simovanev <simovanev@abv.bg>
2024-09-20 04:52:54 +0300

    Include better looking login form

--- src/main/java/com/springboot/springMvcSecurity/controller/LoginController.java
+++ src/main/java/com/springboot/springMvcSecurity/controller/LoginController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     @GetMapping("/loginPage")
     public String login(){
-        return "login-page";
+        return "fancy-login";
     }
 }
